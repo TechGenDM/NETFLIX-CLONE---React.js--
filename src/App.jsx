837d8e88812bf17657e8d99bd8e0f1e25a84724a
devasish <devasish@mishra.pro>
2025-08-28 17:54:05 +0530

    fixed issues

--- src/App.jsx
+++ src/App.jsx
@@ -6,14 +6,13 @@ import Player from './pages/Player/Player'
 
 const App = () => {
   return (
-    <div>
+    <>
       <Routes>
         <Route path='/' element={<Home/>}/>
         <Route path='/login' element={<Login/>}/>
         <Route path='/player/:id' element={<Player/>}/>
       </Routes>
-      <Home/>
-    </div>
+    </>
   )
 }
 
